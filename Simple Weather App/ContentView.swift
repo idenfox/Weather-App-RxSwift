1d3c762e3771479b66faabb00c4728c4f0cdf496
Renzo Paul Chamorro <renzo607@gmail.com>
2021-04-18 14:20:01 -0500

    Initial Commit

---
+++ Simple Weather App/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Simple Weather App
+//
+//  Created by Renzo Paul Chamorro on 18/04/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
